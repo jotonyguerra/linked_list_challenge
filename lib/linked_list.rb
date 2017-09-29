3d0bc78415da31304860673b0c76c44240b47bf2
Jotony Guerra <jotonyguerra@gmail.com>
2017-09-29 12:44:42 -0700

    initial code written

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,5 +2,22 @@ require 'pry'
 require_relative 'node'
 
 class LinkedList
- 
+  def initialize
+    @head = nil
+  end
+
+  def each
+    node = @head
+
+    while !node.nil?
+      yield(node.data)
+      node = node.next
+    end
+  end
+
+  def preprend(value)
+    @head = Node.new(value, @head)
+  end
+
+
 end
